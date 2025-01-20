bbdacfa4a85f268ebb9c82538dbea11b1c6e8d87
sushil <sushil.kumar@infostride.com>
2025-01-20 18:58:27 +0530

    pusehed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: 'window',
+  server: {
+    port: process.env.PORT,
   },
-})
+  plugins: [vercel()],
+});
